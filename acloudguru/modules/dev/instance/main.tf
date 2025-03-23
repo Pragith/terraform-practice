44cab0606643dc4ce5603842d79d18fbb615a111
Pragith Prakash <pragith@gmail.com>
2025-03-23 09:01:03 -0400

    successfully ran terraform init

---
+++ acloudguru/modules/dev/instance/main.tf
@@ -0,0 +1,15 @@
+# --- instance/main.tf --- #
+
+resource "aws_instance" "app_server" {
+  ami = data.aws_ssm_parameter.ec2-ami.value
+  instance_type = var.instance_type
+  subnet_id = var.subnet_id
+  count = var.instance_count
+	tags = {
+		Name = "app-server"
+	}
+}
+
+data "aws_ssm_parameter" "ec2-ami" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
