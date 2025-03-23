4927a781a593aa77255e7e53ae5021897fea2e98
Pragith Prakash <pragith@gmail.com>
2025-03-23 09:01:50 -0400

    Performed terraform fmt -recursive

--- acloudguru/modules/dev/instance/main.tf
+++ acloudguru/modules/dev/instance/main.tf
@@ -1,13 +1,13 @@
 # --- instance/main.tf --- #
 
 resource "aws_instance" "app_server" {
-  ami = data.aws_ssm_parameter.ec2-ami.value
+  ami           = data.aws_ssm_parameter.ec2-ami.value
   instance_type = var.instance_type
-  subnet_id = var.subnet_id
-  count = var.instance_count
-	tags = {
-		Name = "app-server"
-	}
+  subnet_id     = var.subnet_id
+  count         = var.instance_count
+  tags = {
+    Name = "app-server"
+  }
 }
 
 data "aws_ssm_parameter" "ec2-ami" {
--- acloudguru/modules/dev/vpc/outputs.tf
+++ acloudguru/modules/dev/vpc/outputs.tf
@@ -2,41 +2,41 @@
 
 
 output "subnet_id" {
-  value = aws_subnet.instance_subnet.id
+  value       = aws_subnet.instance_subnet.id
   description = "The ID of the subnet"
 }
 
 output "vpc_id" {
-  value = aws_vpc.instance_vpc.id
+  value       = aws_vpc.instance_vpc.id
   description = "The ID of the VPC"
 }
 
 output "route_table_id" {
-  value = aws_route_table.instance_route_table.id
+  value       = aws_route_table.instance_route_table.id
   description = "The ID of the route table"
 }
 
 output "gateway_id" {
-  value = aws_internal_gateway.instance_gateway.id
+  value       = aws_internal_gateway.instance_gateway.id
   description = "The ID of the gateway"
 }
 
 output "subnet_cidr_block" {
-  value = aws_subnet.instance_subnet.cidr_block
+  value       = aws_subnet.instance_subnet.cidr_block
   description = "The CIDR block of the subnet"
 }
 
 output "vpc_cidr_block" {
-  value = aws_vpc.instance_vpc.cidr_block
+  value       = aws_vpc.instance_vpc.cidr_block
   description = "The CIDR block of the VPC"
 }
 
 output "availability_zone" {
-  value = aws_subnet.instance_subnet.availability_zone
+  value       = aws_subnet.instance_subnet.availability_zone
   description = "The availability zone of the subnet"
 }
 
 output "route_table_association_id" {
-  value = aws_route_table_association.instance_route_table_association.id
+  value       = aws_route_table_association.instance_route_table_association.id
   description = "The ID of the route table association"
 }
